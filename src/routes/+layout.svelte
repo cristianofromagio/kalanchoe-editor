01531a9ccfcb63b6f2a2b38d8a0800bd85308d70
Cristiano Fromagio <cristianofromagio@gmail.com>
2024-02-07 00:44:06 -0300

    feat: tailwind install

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	import "../app.css";
+</script>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+	}
+</style>
+
+<header>
+	<h1>Kalanchoe PlantUML Editor</h1>
+	<nav>
+		<a href="/">Index</a>
+		<a href="/new">New diagram</a>
+	</nav>
+</header>
+
+<slot />
